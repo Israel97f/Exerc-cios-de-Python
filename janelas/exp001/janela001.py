0bd3b828289ed21b221c3ebcb24241f88a9bb8b8
Israel Farias <israelfarias97@gmail.com>
2021-12-08 23:57:05 -0300

    update

--- janelas/exp001/janela001.py
+++ janelas/exp001/janela001.py
@@ -1,9 +1,20 @@
 from tkinter import Tk, Label, Button
+i = 1
+def muda_label(evento):
+    global i
+    texto.config(text=f'apetou {i}')
+    i += 1
+
+def muda_botao():
+    print('fui clicado')
+    butao.config(text='fui clicado')
 
 janela = Tk()
 
 butao = Button(
-    text = 'Click-me'
+    text = 'Click-me',
+    font=('Arial', 12),
+    command=muda_botao
 )
 butao.pack()
 
@@ -13,5 +24,5 @@ texto = Label(
 )
 
 texto.pack()
-
+janela.bind('1', muda_label)
 janela.mainloop()
