5ea9aeb58090a49078bdbcdb8a3e057d0892fbb8
Israel Farias <israelfarias97@gmail.com>
2021-12-09 16:57:54 -0300

    update

--- janelas/exp003/janela003.py
+++ janelas/exp003/janela003.py
@@ -1,11 +1,8 @@
-from tkinter import Tk, ttk
-from googletrans import Translator
+from googletrans import Translator 
 
-tradutor = Translator()
-resutado = tradutor.translate(
-    'Ola mundo',
-    src='pt',
-    dest='en'
-)
-
-print(resutado.text)
+translator = Translator()
+translator.translate('안녕하세요.')
+# <Translated src=ko dest=en text=Good evening. pronunciation=Good evening.>
+translator.translate('안녕하세요.', dest='ja')
+# <Translated src=ko dest=ja text=こんにちは。 pronunciation=Kon'nichiwa.>
+translator.translate('veritas lux mea', src='la')
