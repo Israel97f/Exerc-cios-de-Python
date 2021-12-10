6c291961cc6565c7026974b4b333bcece2ff1f71
Israel Farias <israelfarias97@gmail.com>
2021-12-09 22:03:26 -0300

    update
    Janela n 3 foi criada

--- janelas/exp003/janela003.py
+++ janelas/exp003/janela003.py
@@ -1,8 +1,47 @@
-from googletrans import Translator 
-
-translator = Translator()
-translator.translate('안녕하세요.')
-# <Translated src=ko dest=en text=Good evening. pronunciation=Good evening.>
-translator.translate('안녕하세요.', dest='ja')
-# <Translated src=ko dest=ja text=こんにちは。 pronunciation=Kon'nichiwa.>
 translator.translate('veritas lux mea', src='la')
+from tkinter import Tk, ttk, Text
+from googletrans import Translator
+
+
+def traduzir():
+    text= entrada_texto.get('1.0', 'end')
+    dest= saida.get()
+    src= entrada.get()
+    translator = Translator()
+    resultado = translator.translate(text=text, dest=f'{dest}', src=f'{src}')
+
+    saida_texto.configure(state='normal')
+    saida_texto.delete('1.0', 'end')
+    saida_texto.insert('1.0', resultado.text)
+    saida_texto.configure(state='disabled')
+
+
+janela = Tk()
+janela.title('Boogle Translator')
+
+# ------ Entradas ---------
+entrada_frame = ttk.Frame()
+entrada_label = ttk.Label(entrada_frame, text='entrada', font=('Roboto', 16))
+entrada = ttk.Combobox(entrada_frame, values=('en', 'es', 'pt', 'ja'), font=('Roboto',16))
+entrada.set('pt')
+entrada_label.grid(row=0, column=0, padx=10, pady=10)
+entrada.grid(row=0, column=1)
+entrada_frame.pack()
+entrada_texto = Text(height=10, width=50, font=('Roboto', 16))
+entrada_texto.pack(padx=10, fill='both', expand='yes')
+
+# ------- Saidas ----------
+saida_frame = ttk.Frame()
+saida_label = ttk.Label(saida_frame, text='Saida', font=('Roboto', 16))
+saida = ttk.Combobox(saida_frame, values=('en', 'es', 'pt', 'ja'), font=('Roboto',16))
+saida.set('en')
+saida_label.grid(row=0, column=0, padx=10, pady=10)
+saida.grid(row=0, column=1)
+saida_frame.pack()
+saida_texto = Text(height=10, width=50, font=('Roboto', 16), state='disabled')
+saida_texto.pack(padx=10, fill='both', expand='yes')
+
+botao = ttk.Button(text='Traduzir', command=traduzir)
+botao.pack(fill='both', padx=10, pady=10)
+
+janela.mainloop()
